91b7ae9f37a04440a55a19872bd078bed3e6edeb
vitalina.romanova <vitalina.romanova@sbermarket.ru>
2023-04-04 22:23:32 +0300

    Ex5: Парсинг JSON

--- json_parsing.py
+++ json_parsing.py
@@ -1,11 +1,5 @@
 import json
 
-string_as_json_format = '{"answer": "Hello, User"}'
-obj = json.loads(string_as_json_format)
-
-key = "answer"
-
-if key in obj:
-    print(obj[key])
-else:
-    print(f"Нет {key}")
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
+print(obj['messages'][1]['message'])
