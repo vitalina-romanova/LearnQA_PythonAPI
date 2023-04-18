70a8fa2f7f044e75c6ee5a815d1f2a88fa0c0f7f
vitalina.romanova <vitalina.romanova@sbermarket.ru>
2023-04-18 21:11:09 +0300

    test_create_user_successfully

--- lib/assertions.py
+++ lib/assertions.py
@@ -12,3 +12,17 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_massage
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecoderError:
+            assert False, f"Response doesn't have is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected {expected_status_code}. Actual: {response.status_code}"
